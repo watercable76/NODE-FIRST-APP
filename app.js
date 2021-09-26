70591824384c3f884b508115fab6830bfd787412
Nicholas Cable <cab17001@byui.edu>
2021-09-25 22:41:27 -0600

    Cleaned up code and made deployment to heroku easier

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // import files
 const adminRoutes = require('./routes/admin');
@@ -23,6 +24,21 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// adding features to support Heroku development
+const corsOptions = {
+    origin: "https://node-first-app-cse341.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || `mongodb+srv://${Login.username}:${Login.pass}@cluster0.lwwzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 app.use((req, res, next) => {
     User.findById('614fdfe1e754c102d7582f37')
         .then(user => {
@@ -37,7 +53,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(`mongodb+srv://${Login.username}:${Login.pass}@cluster0.lwwzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+mongoose
+    .connect(
+        MONGODB_URL, options
+    )
     .then(result => {
         User.findOne().then(user => {
             if (!user) {
